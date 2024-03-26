5ee921e441f7ff8d89dbb42d995b111be755eaf7
MalaviyaIsha <isha.malaviya@exhibytesolution.com>
2024-03-26 18:46:26 +0530

    Introduction about React Leaflet , Install and setup Leaflet , display basic map using react leaflet that includes MapContainer , TileLayer , Marker , Tooltip , SVGOverlay , LayersControl.

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Dashboard from "./container/Dashboard/index.tsx";
 import ButtonComponent from "./components/Button/index.tsx";
 import ButtonStyle from "./components/Button/buttonstyle1.tsx";
 import FirstComponent from "./components/ChildrenPass/FirstComponent.tsx";
+import DisplayMap from "./components/LeafletMap/Map.tsx";
 
 const App: React.FC = () => (
   <div className="App">
     {/* <Dashboard /> */}
     {/* <ButtonStyle/> */}
-    <FirstComponent/>
+    {/* <FirstComponent/> */}
+    <DisplayMap/>
   </div>
 );
 
---
+++ src/components/LeafletMap/Map.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { LayersControl, MapContainer, Marker, Popup, SVGOverlay, TileLayer, Tooltip } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./styles.scss";
+import { Icon } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+
+interface MarkerType {
+  geocode: [number, number];
+  popUp: string;
+}
+
+interface BoundType {
+  bounds: [[number, number], [number, number]];
+}
+
+const DisplayMap: React.FC = () => {
+  const markers: MarkerType[] = [
+    {
+      geocode: [51.505, -0.09],
+      popUp: "Hello, I am PopUp 1",
+    },
+    {
+      geocode: [51.505, -0.05],
+      popUp: "Hello, I am PopUp 2",
+    },
+    {
+      geocode: [51.505, -0.07],
+      popUp: "Hello, I am PopUp 3",
+    },
+  ];
+
+  const customIcon = new Icon({
+    // iconUrl: '',
+    iconUrl: require("../../assets/location-icon.png"),
+    iconSize: [39, 39],
+  });
+
+  const bounds: BoundType = {
+    bounds: [
+      [51.49, -0.08],
+      [51.5, -0.06],
+    ],
+  };
+
+  return (
+    <div className="main-div">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <MarkerClusterGroup chunkedLoading>
+          {markers.map(({ geocode, popUp }) => (
+            <Marker position={geocode} icon={customIcon}>
+              <Popup>{popUp}</Popup>
+              <Tooltip>Tooltip for Marker</Tooltip>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+
+        <SVGOverlay attributes={{ stroke: "black" }} bounds={bounds.bounds}>
+          <rect x="0" y="0" width="100%" height="100%" fill="cyan" />
+          <circle r="5" cx="10" cy="10" fill="red" />
+          <text x="50%" y="50%" stroke="black">
+            text
+          </text>
+        </SVGOverlay>
+
+        <LayersControl position="topright">
+          <LayersControl.Overlay name="Marker with popup1">
+            <Marker position={[51.505, -0.03]} icon={customIcon}>
+              <Popup>
+                A pretty CSS3 popup.
+              </Popup>
+            </Marker>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name="Marker with popup2">
+            <Marker position={[51.505, -0.06]} icon={customIcon}>
+              <Popup>
+                A pretty CSS3 popup.
+              </Popup>
+            </Marker>
+          </LayersControl.Overlay>
+        </LayersControl>
+      </MapContainer>
+
+    </div>
+  );
+};
+export default DisplayMap;
